b60781c66e22932c3a094fdd60c6578b0bd020e6
alis1 <example.com>
2025-05-29 19:32:16 +0200

    network

---
+++ app-startup network.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal
+
+where python >nul 2>&1
+if errorlevel 1 (
+    echo Python nie jest zainstalowany lub nie jest w PATH.
+    pause
+    exit /b 1
+)
+
+python -m venv .venv
+
+call .venv\Scripts\activate
+
+
+if exist requirements.txt (
+    pip install -r requirements.txt
+) else (
+    echo Brak pliku requirements.txt
+    pause
+    exit /b 1
+)
+
+flask --app app run -h 192.168.1.129 --debug
+
+
+pause
+
+deactivate
+
+endlocal
+
+pause
+
